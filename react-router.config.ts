7d1dbc19207d02c8cec2b8aca29f24241cefda29
phuockaito <huuphuocit1999@gmail.com>
2025-08-21 15:55:32 +0700

    build

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,5 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-    // Config options...
-    // Server-side render by default, to enable SPA mode set this to `false`
     ssr: true,
-    prerender: async ({ getStaticPaths }) => {
-        const paths = await getStaticPaths();
-        return ["/", ...paths];
-    },
 } satisfies Config;
