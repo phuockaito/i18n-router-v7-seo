c5af44daab7cb8d85b973cade960853161daddc3
phuockaito <huuphuocit1999@gmail.com>
2025-08-22 10:15:53 +0700

    build

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
     ssr: true,
-    prerender: true,
+    prerender: () => {
+        return ["/"];
+    },
 } satisfies Config;
