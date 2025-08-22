72f3ee394bc2453a1d888019a0d4aa57c74038ca
phuockaito <huuphuocit1999@gmail.com>
2025-08-22 09:48:00 +0700

    build

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
     ssr: true,
     prerender: () => {
-        return ["/", "/vi/gioi-thieu", "/en/about-us"];
+        return ["/"];
     },
 } satisfies Config;
