7308e6fdd5f85ad24b2eeb45279c552a473ba68a
phuockaito <huuphuocit1999@gmail.com>
2025-08-27 10:26:33 +0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import devtoolsJson from 'vite-plugin-devtools-json';
 import tsconfigPaths from "vite-tsconfig-paths";
-
-
-// export default defineConfig({
-//     plugins: [tsconfigPaths(), tailwindcss(), reactRouter(), devtoolsJson()],
-//     server: { port: 8080 },
-// });
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(() => {
     return {
         plugins: [tsconfigPaths(), tailwindcss(), reactRouter(), devtoolsJson()],
         server: {
             port: 8080,
-            proxy: {
-                "/api": {
-                    target: env.VITE_APP_URL_API,
-                    changeOrigin: true,
-                    rewrite: (path) => path.replace(/^\/api/, ""),
-                },
-            },
         },
     }
 })
